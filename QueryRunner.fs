6ff74c80a000d7f56399caa87405acdb369da3d7
Piotr Kosiński <Piotr.Kosinski@powel.com>
2015-07-10 12:12:48 +0200

    Init

---
+++ QueryRunner.fs
@@ -0,0 +1,52 @@
+﻿module QueryRunner
+    type AggregateFunction = Intersect | Union
+    type Query = 
+        | SimpleQuery of string
+        | ComplexQuery of AggregateFunction * Query list
+
+    let aggregateResults aggregate =
+            match aggregate with
+            | Intersect -> Set.intersectMany
+            | Union -> Set.unionMany
+
+    let rec runQuery executor query = 
+        match query with 
+        | SimpleQuery q -> executor q
+        | ComplexQuery (aggregateFunction,queries) -> 
+                                            queries
+                                                |> Seq.map (fun q -> runQuery executor q)
+                                                |> Set.ofSeq
+                                                |> aggregateResults aggregateFunction
+    
+    #nowarn "40"
+
+    type countMsg =
+    | Die
+    | Incr of int
+    | Fetch of AsyncReplyChannel<int>
+
+    let printerAgent = MailboxProcessor.Start(fun inbox-> 
+        // the message processing function
+        let rec messageLoop = async{
+            // read a message
+            let! msg = inbox.Receive()
+        
+            // process a message
+            match msg with
+            | Die -> return ()
+            | Incr x -> return! messageLoop
+            | Fetch(reply) ->
+                reply.Reply(1);
+                return! messageLoop
+
+            // loop to top
+            return! messageLoop  
+            }
+
+        // start the loop 
+        messageLoop 
+    )
+
+    let compute = 
+        printerAgent.PostAndAsyncReply (fun reply -> Fetch(reply))
+        |> Async.RunSynchronously
---
+++ QueryRunnerTests.fs
@@ -0,0 +1,48 @@
+﻿module QueryTests
+
+open NUnit.Framework
+open Swensen.Unquote
+open QueryRunner
+open System.Threading
+
+let slowExecutor input = 
+    Thread.Sleep(100)
+    Set[input]
+
+[<Test>]
+let ``simple query`` () =
+    let query = SimpleQuery("obligation1")
+    let result = runQuery slowExecutor query
+    result =? Set["obligation1"]
+
+[<Test>]
+let ``complex query with OR aggregate`` () =
+    let simpleQuery1 = SimpleQuery("obligation1")
+    let simpleQuery2 = SimpleQuery("obligation2")
+
+    let complexQuery = ComplexQuery(AggregateFunction.Union,[simpleQuery1;simpleQuery2])
+    let result = runQuery slowExecutor complexQuery
+
+    result =? Set["obligation1";"obligation2"]
+
+[<Test>]
+let ``complex query with AND aggregate`` () =
+    let simpleQuery1 = SimpleQuery("obligation1")
+    let simpleQuery2 = SimpleQuery("obligation2")
+
+    let complexQuery = ComplexQuery(AggregateFunction.Intersect,[simpleQuery1;simpleQuery2])
+    let result = runQuery slowExecutor complexQuery
+
+    result =? Set[]  
+
+[<Test>]
+let ``complex query with Intersect and Union aggregate`` () =
+    let simpleQuery1 = SimpleQuery("obligation1")
+    let simpleQuery2 = SimpleQuery("obligation2")
+
+    let complexQuery = ComplexQuery(AggregateFunction.Union,[simpleQuery1; simpleQuery1; simpleQuery2 ])
+    let finalQuery = ComplexQuery(AggregateFunction.Intersect,[simpleQuery1;complexQuery])
+
+    let result = runQuery slowExecutor finalQuery
+
+    result =? Set["obligation1"] 
